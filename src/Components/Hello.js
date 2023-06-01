d704af955936dc61757fcf8a3131c7662010a6d5
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 13:21:39 +0530

    Add. Class component and alternative of JSX

---
+++ src/Components/Hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hello = () => {
+	return React.createElement("div",{id:"id", className:"Hello"}, React.createElement("h1", null, "Hello Hinduja"));
+};
+
+export default Hello;
