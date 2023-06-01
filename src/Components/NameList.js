17042a6ea50f7c1629fe897df9f48825f0dedfe0
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 20:26:17 +0530

    List of objects rendering

--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 function NameList() {
-	const names = ["Hinduja", "Saketh", "Shashi"];
-	const nameList = names.map((name) => <h2>{name}</h2>);
-	return <div>{nameList}</div>;
+	const persons = [
+		{ id: 1, name: "Hinduja", age: 2, skill: "React" },
+		{ id: 2, name: "Saketh", age: 1, skill: "Vue" },
+		{ id: 3, name: "Shashi", age: 0, skill: "Angular" },
+	];
+	const personList = persons.map((person) => (
+		<h2>
+			I am {person.name}, I am {person.age} years old, I am skilled at {person.skill}
+		</h2>
+	));
+	return <div>{personList}</div>;
 }
 
 export default NameList;
