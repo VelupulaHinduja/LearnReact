2409bae37574faa3f10fa74947d83201a007ef53
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 15:56:26 +0530

    Pass func as argument when there is prev state dependency

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -8,18 +8,21 @@ class Counter extends Component {
 		};
 	}
 	increment() {
-		this.setState(
-			{
-				count: this.state.count + 1,
-			},
-			() => console.log(this.state.count)
-		);
+		this.setState(prevState => ({ count: prevState.count + 1 }))
+        console.log(this.state.count)
+	}
+	incrementFive() {
+		this.increment();
+		this.increment();
+		this.increment();
+		this.increment();
+		this.increment();
 	}
 	render() {
 		return (
 			<div>
 				<h3>Count {this.state.count}</h3>
-				<button onClick={() => this.increment()}>Increment</button>
+				<button onClick={() => this.incrementFive()}>Increment</button>
 			</div>
 		);
 	}
