38f7f925517631298a48a52ceeb2df70d14a3016
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 13:20:36 +0530

    Add. ALternative of JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Greet from "./Components/Greet";
+import Hello from "./Components/Hello";
+import Welcome from "./Components/Welcome";
 
 function App() {
 	return (
 		<div className="App">
-			<Greet />
+			{/* <Greet /> */}
+      {/* <Welcome/> */}
+      <Hello/>
 		</div>
 	);
 }
