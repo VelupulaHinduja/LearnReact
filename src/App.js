05e3f00cba3a0653c3be989a6719d6c913ed80e8
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 16:13:34 +0530

    Func component event handling

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Greet from "./Components/Greet";
 import Hello from "./Components/Hello";
 import Message from "./Components/Message";
 import Welcome from "./Components/Welcome";
+import FunctionClick from "./FunctionClick";
 
 function App() {
 	return (
 		<div className="App">
-			<Greet name="Hinduja" nickName="Hindu"></Greet>
-      <Welcome name="Hinduja" nickName="Hindu"></Welcome>
+			{/* <Greet name="Hinduja" nickName="Hindu"></Greet> */}
+      {/* <Welcome name="Hinduja" nickName="Hindu"></Welcome> */}
       {/* <Hello/> */}
       {/* <Message/> */}
       {/* <Counter/> */}
+      <FunctionClick/>
 		</div>
 	);
 }
