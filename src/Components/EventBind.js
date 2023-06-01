b6327c887ac072c7ea0ab0c031d70d85de66fb8e
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 19:00:32 +0530

    Event binding approach 1(performance implications)

--- src/Components/EventBind.js
+++ src/Components/EventBind.js
@@ -11,12 +11,13 @@ class EventBind extends Component {
 		this.setState({
 			message: "Good Bye",
 		});
+        console.log(this)
 	}
 	render() {
 		return (
 			<div>
 				<h1>{this.state.message}</h1>
-				<button onClick={this.changeMessage}>Click</button>
+				<button onClick={this.changeMessage.bind(this)}>Click</button>
 			</div>
 		);
 	}
