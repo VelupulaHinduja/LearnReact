50f1362d58503dc4a71900ae6652d2ad3f4f24a8
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 15:02:16 +0530

    Add. state variable in class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Greet from "./Components/Greet";
 import Hello from "./Components/Hello";
+import Message from "./Components/Message";
 import Welcome from "./Components/Welcome";
 
 function App() {
 	return (
 		<div className="App">
-			<Greet name="Hinduja" nickName="Hindu">This is a children prop</Greet>
-      <Welcome name="Hinduja" nickName="Hindu">Children prop from class component</Welcome>
+			{/* <Greet name="Hinduja" nickName="Hindu">This is a children prop</Greet> */}
+      {/* <Welcome name="Hinduja" nickName="Hindu">Children prop from class component</Welcome> */}
       {/* <Hello/> */}
+      <Message/>
 		</div>
 	);
 }
