26e6c66c90f34e882c409f6cd4b260d8c0c60928
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-01 19:07:43 +0530

    Event binding approach 4 handler as arrow function

--- src/Components/EventBind.js
+++ src/Components/EventBind.js
@@ -6,20 +6,26 @@ class EventBind extends Component {
 		this.state = {
 			message: "Hello",
 		};
-        this.changeMessage = this.changeMessage.bind(this)
+		// this.changeMessage = this.changeMessage.bind(this)
 	}
-	changeMessage() {
-		this.setState({
-			message: "Good Bye",
-		});
-		console.log(this);
-	}
+	// changeMessage() {
+	// 	this.setState({
+	// 		message: "Good Bye",
+	// 	});
+	// 	console.log(this);
+	// }
+
+	changeMessage = () => {
+		this.setState({ message: "Good Bye" });
+        console.log(this)
+	};
 	render() {
 		return (
 			<div>
 				<h1>{this.state.message}</h1>
 				{/* <button onClick={this.changeMessage.bind(this)}>Click</button> */}
 				{/* <button onClick={() => this.changeMessage()}>Click</button> */}
+				{/* <button onClick={this.changeMessage}>Click</button> */}
 				<button onClick={this.changeMessage}>Click</button>
 			</div>
 		);
