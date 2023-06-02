0a16406a7ee684e7ea2a448b2304485e2f0ac261
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-02 14:59:16 +0530

    Add 2 class names in css styling

--- src/Components/StyleSheets.js
+++ src/Components/StyleSheets.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import "./myStyles.css"
+import React from "react";
+import "./myStyles.css";
 
 function StyleSheets(props) {
-    const className = props.primary ? "primary": ""
-  return (
-    <h1 className={className}>StyleSheets</h1>
-  )
+	const className = props.primary ? "primary" : "";
+	return <h1 className={`${className} font-xl`}>StyleSheets</h1>;
 }
 
-export default StyleSheets
+export default StyleSheets;
