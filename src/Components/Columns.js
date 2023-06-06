92c5f8fec24c7dbacb6d8cd12ad666d772f1f1f9
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-06 11:48:45 +0530

    Use empty tags instead of fragments but can't pass key as a prop

--- src/Components/Columns.js
+++ src/Components/Columns.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Columns() {
   return (
-    <React.Fragment>
+    <>
         <td>Name</td>
         <td>Hinduja</td>
-    </React.Fragment>
+    </>
   )
 }
 
