903171b722b38ededb9392b11ffe3a9ebae63edc
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-06 19:19:59 +0530

    ComponentDidCatch use to log the error and info of error

--- src/Components/ErrorBoundary.js
+++ src/Components/ErrorBoundary.js
@@ -11,6 +11,10 @@ class ErrorBoundary extends Component {
 	static getDerivedStateFromError(error) {
 		return { hasError: true };
 	}
+    componentDidCatch(error,info){
+        console.log(error)
+        console.log(info)
+    }
 	render() {
 		if (this.state.hasError) {
 			return <h1>Something went wrong</h1>;
