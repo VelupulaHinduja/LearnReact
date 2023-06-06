484c6b90945d682e1c45a4ba563c1d7b4373dca0
VelupulaHinduja <hinduja.velpula@gmail.com>
2023-06-06 11:47:39 +0530

    Fix. warning to use React.Fragment instead of div

--- src/Components/Columns.js
+++ src/Components/Columns.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Columns() {
   return (
-    <div>
+    <React.Fragment>
         <td>Name</td>
         <td>Hinduja</td>
-    </div>
+    </React.Fragment>
   )
 }
 
